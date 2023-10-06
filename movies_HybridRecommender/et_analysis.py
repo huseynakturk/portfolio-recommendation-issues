3893acd37bb3239bf5754a0f9420b580878a9583
Huseyin Akturk <107130043+huseynakturk@users.noreply.github.com>
2023-10-07 00:21:23 +0300

    Add files via upload

---
+++ movies_HybridRecommender/et_analysis.py
@@ -0,0 +1,127 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# EDA functions version 1.0 (basic)
+
+# Genel Resim
+
+def check_df(dataframe,head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### NA #####################")
+    print(pd.DataFrame(dataframe.isnull().sum(), columns = ["# of N/A values"]))
+    print("##################### Quantiles #####################")
+    print(pd.DataFrame(dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T))
+
+
+# kategorik değişken analizi
+
+def cat_summary(datframe,col_name,plot=False):
+    if datframe[col_name].dtypes  == "bool":
+        datframe[col_name] = datframe[col_name].astype(int)
+        
+    print(pd.DataFrame({col_name: datframe[col_name].value_counts(),
+                        "Ratio" : 100* datframe[col_name].value_counts(normalize=True)}))
+    
+    if plot:
+        sns.countplot(x=datframe[col_name],data=datframe)
+        plt.show(block=True)
+    
+    
+# numeric değişken analizi
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist()
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+# Değişkenlerin Yakalanması
+def grab_col_names(df, cat_th=10,  car_th=20):
+    """
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+
+    Parameters
+    ----------
+    dataframe: dataframe
+        değişken isimleri alınmak istenen dataframe'dir.
+    cat_th: int, float
+        numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+    car_th: int, float
+        kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    -------
+    cat_cols: list
+        Kategorik değişken listesi
+    num_cols: list
+        Numerik değişken listesi
+    cat_but_car: list
+        Kategorik görünümlü kardinal değişken listesi
+
+    Notes
+    ------
+    cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+    num_but_cat cat_cols'un içerisinde.
+
+    """
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+
+    num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int64", "float64"]]
+
+    cat_but_car = [col for col in df.columns if
+                   df[col].nunique() > 20 and str(df[col].dtypes) in ["category", "object"]]
+
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in df.columns if df[col].dtypes in ["int64", "float64"]]
+    num_cols = [col for col in num_cols if col not in cat_cols]
+
+    print(f"Observations: {df.shape[0]}")
+    print(f"Variables: {df.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+
+# Hedef Değişkenin Kategorik Değişkenler ile Analizi
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+# Hedef Değişkenin Numeric Değişkenler ile Analizi
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+
+
+# Yüksek Korelasyonlu Değişkenlerin Silinmesi
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+
+
+
+
